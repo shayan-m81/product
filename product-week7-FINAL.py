57ddcd308c015dfbd133867f234231c1131203bd
shayan-m81 <95168195+shayan-m81@users.noreply.github.com>
2021-11-28 19:58:22 +0330

    Update and rename product-week7FINAL.py to product-week7-FINAL.py

--- product-week7-FINAL.py
+++ product-week7-FINAL.py
@@ -1,7 +1,9 @@
-first_input = input().split()
+# to inputs disk_numbers and steps_count
+first_input = input().split()     
 disk_number = int(first_input[0])
 shape_list = []
 
+# these 4 fors are to fill rows of my shape based on disk_numbers
 for x in range(disk_number + 3):
     shape_list.append(["", "", "", ""])
 
@@ -14,18 +16,21 @@ for x in range(3):
 
 for i in range(disk_number):
     shape_list[disk_number + 1 - i][0] = "==" * (disk_number - i)
+    
 shape_list.append(["Tower #0", "Tower #1", "Tower #2"])
 
 
+# this def will draw my shape 
 def print_shape():
     for f in range(len(shape_list) * 4):
-        if (f % 4) != 3:
-            print(shape_list[f // 4][f % 4].center(20, " "), end="")
+        if (f % 4) != 3:    # to consider first 3 indices of each row
+            print(shape_list[f // 4][f % 4].center(20, " "), end="")    # to move on each row's indices
             print(" ", end="")
         else:
             print("")
 
 
+# this def will find the number of row that i should make change in, for each columns
 def top(column):
     for g in range(disk_number + 3):
         if shape_list[g][column][0] == "=":
@@ -33,9 +38,11 @@ def top(column):
         elif g == disk_number + 2:
             return disk_number + 2
 
-
+        
 steps = int(first_input[-1])
 print_shape()
+
+# to change the position of disks for each move based the inputs
 for x in range(steps):
     command = list(input())
     column1 = int(command[0])
